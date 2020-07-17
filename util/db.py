e14936085f6b9669ae3b260058a9d275476e106a
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2020-07-16 21:03:18 -0700

    More automation to db.py

--- util/db.py
+++ util/db.py
@@ -7,13 +7,17 @@ import pymongo
 import os
 
 
-def insert_collection_doc_data(filepath, filename, indexes=[]):
+def insert_collection_doc_data(
+    filepath, filename, indexes=[], collection_name=None
+):
+    if not collection_name:
+        collection_name = filename.replace(".json", "")
+
     client = pymongo.MongoClient()
     db = client.cyberpunk2020
 
     with open(os.path.join(filepath, filename), "r") as f:
         data = json.loads(f.read())
-        collection_name = filename.replace(".json", "")
 
         # Murder any existing collection and rebuild it.
         # This function should preserve nothing, and replace everything.
@@ -24,3 +28,25 @@ def insert_collection_doc_data(filepath, filename, indexes=[]):
         if indexes:
             for index in indexes:
                 collection.create_index(index)
+
+
+dataPath = "cyberpunk_discord_bot/data/"
+insert_collection_doc_data(
+    dataPath, "career_skills.json", ["stat", "role_name"]
+)
+insert_collection_doc_data(
+    dataPath, "master_skill_list.json", ["stat"], "skills"
+)
+insert_collection_doc_data(dataPath, "lifepath.json", ["step", "table_name"])
+insert_collection_doc_data(
+    dataPath,
+    "weapons.json",
+    [
+        "category",
+        "type",
+        "concealability",
+        "availability",
+        "ammo_type",
+        "reliability",
+    ],
+)
