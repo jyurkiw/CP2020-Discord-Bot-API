149f7f0efa033e7b1727b03c60cf89585a2fe9ac
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2020-07-21 18:34:02 -0700

    Fixed some array shenanigains

--- api/equipment/armor.py
+++ api/equipment/armor.py
@@ -18,7 +18,7 @@ class ArmorRoller(MongoHandler):
 
     @staticmethod
     def getArmorFilter(armorList):
-        protectedLocations = ArmorRoller.getProtectedLocations(armor)
+        protectedLocations = ArmorRoller.getProtectedLocations(armorList)
         return {
             "$and": [
                 {location: False}
@@ -34,21 +34,24 @@ class ArmorRoller(MongoHandler):
         if maxRolls == 0:
             return []
 
-        armor = self.getRandom({})
+        armor = [self.getRandom({})]
 
         if maxRolls == 1:
             return armor
 
         protectedLocations = ArmorRoller.getProtectedLocations(armor)
-        armor += self.getRandom(ArmorRoller.getArmorFilter(armor))
+        armor += [self.getRandom(ArmorRoller.getArmorFilter(armor))]
 
         if maxRolls == 2:
             return armor
 
         protectedLocations = ArmorRoller.getProtectedLocations(armor)
-        armor += self.getRandom(ArmorRoller.getArmorFilter(armor))
+        armor += [self.getRandom(ArmorRoller.getArmorFilter(armor))]
 
-        return armor
+        # Filter the return value because if no armor can be selected
+        # (for example, if choice #1 was metal gear) then getRandom
+        # will return None and [None] will be added to the armorList.
+        return [a for a in armor if a]
 
     def getRandomArmor(self):
         """Returns a list of armor between 1 and 3 items long (random range).
